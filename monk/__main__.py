9e80b565241afa49700eda24f4772a3b6e9486e5
interrrp <interrrp@proton.me>
2025-05-12 21:10:23 +0800

    Support running files

--- monk/__main__.py
+++ monk/__main__.py
@@ -1,17 +1,26 @@
+from pathlib import Path
+from sys import argv
+
 from monk.evaluator import evaluate
 from monk.lexer import Lexer
 from monk.object import Environment
 from monk.parser import Parser
 
+
+def run(code: str, env: Environment) -> None:
+    lexer = Lexer(code)
+    parser = Parser(lexer)
+    program = parser.parse_program()
+    result = evaluate(program, env)
+    print(result)  # noqa: T201
+
+
 if __name__ == "__main__":
     env = Environment()
 
-    while True:
-        code = input(">>> ")
-        lexer = Lexer(code)
-
-        parser = Parser(lexer)
-        program = parser.parse_program()
-
-        result = evaluate(program, env)
-        print(result)  # noqa: T201
+    if len(argv) > 1:
+        code = Path(argv[1]).read_text()
+        run(code, env)
+    else:
+        while True:
+            run(input(">>> "), env)
